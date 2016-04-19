78e8f21e3af36287aea40516d1b029e983eb4ea3
Jessica Martin <jesmrtn11@gmail.com>
2016-04-19 14:07:52 +0200

    Skapade kort och deras  värde

--- Deck.class.php
+++ Deck.class.php
@@ -1,16 +1,18 @@
-<?php 
+<?php
 	class Deck {
 		private $deck = [];
 		private $suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
-		private $ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
-		"Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+		private $ranks = ["Ace" => 1, "Two" => 2, "Three" => 3, "Four" => 4, "Five" => 5, "Six" => 6, "Seven" => 7,
+		"Eight" => 50, "Nine" => 9, "Ten" => 10, "Jack" => 10, "Queen" => 10, "King" => 10];
 
 		function __construct() {
 		  foreach ($this->suits as $suit) {
-		    foreach ($this->ranks as $rank) {
-		    	// Create new cards
-		    	$this->deck = new Card($suit, $rank);
-		    }
+		    // Create new cards
+	    	foreach($this->ranks as $rank => $value) {
+	    		$this->deck = new Card($suit, $rank, $value);
+   				// echo "Key=" . $rank . ", Value=" . $value;
+   				// echo "<br>";
+				}
 		  }
 		}
 
@@ -21,6 +23,6 @@
 		function topCard() {
 
 		}
-		
+
 	}
 ?>
