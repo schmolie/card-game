6a1a6c66eb767bfbf25640e3976cd3d84277c779
Jessica Martin <jesmrtn11@gmail.com>
2016-04-19 15:05:44 +0200

    deck fil

--- Deck.class.php
+++ Deck.class.php
@@ -5,7 +5,6 @@
 		private $ranks = ["Ace" => 1, "Two" => 2, "Three" => 3, "Four" => 4, "Five" => 5, "Six" => 6, "Seven" => 7,
 		"Eight" => 50, "Nine" => 9, "Ten" => 10, "Jack" => 10, "Queen" => 10, "King" => 10];
 
-
 		function __construct() {
 		  foreach ($this->suits as $suit) {
 		    // Create new cards
